bb85cae1180994678db2605318b4b177c0374589
harry <316361680@qq.com>
2021-01-11 20:57:07 +0800

    1

---
+++ bandit10.py
@@ -0,0 +1,30 @@
+import random
+import numpy as np
+import matplotlib.pyplot as plt
+qstar=np.random.randn(1,10)
+def bandit(a):
+    """
+    赌博机反馈价值函数
+    """
+    t=qstar[0][a]
+    return np.random.normal(t,1)
+Q=[]
+N=[]
+for i in range(10):
+    Q.append(0)
+    N.append(0)
+epsi=0.1
+rl=[]
+for i in range(2000):
+    if random.uniform(0.0,1.0)<epsi:
+        a=random.randint(0,10)
+    else:
+        t=max(Q)
+        a=Q.index(t)
+    r=bandit(a)
+    rl.append(r)
+    N[a]+=1
+    Q[a]+=(1/N[a])*(r-Q[a])
+
+plt.plot(rl)
+plt.show()
