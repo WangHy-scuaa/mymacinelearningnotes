84057ee1e47339039f1e36fab7cdc0bccc45fd41
harry <316361680@qq.com>
2021-04-16 21:44:05 +0800

    0416

--- dqn/dqntest1.py
+++ dqn/dqntest1.py
@@ -51,7 +51,7 @@ class Estimator(object):
         # 映射像素值大小
         X=tf.cast(self.x_pl,tf.float32)/255.0 
         bat_size=tf.shape(self.x_pl)[0]
-        model=tf.keras.Sequential([
+        model=tf.keras.Model([
             # 卷积层
             tf.keras.layers.Conv2D(32,8,4,activation='relu'),
             tf.keras.layers.Conv2D(64,4,2,activation='relu'),
@@ -61,30 +61,34 @@ class Estimator(object):
             tf.keras.layers.Dense(512),
             tf.keras.layers.Dense(len(VALID_ACTIONS))
         ])
-        self.predictions=model.outputs
+        self.predictions=model(X)#执行模型的输出
         # 进行检验输出
         print(self.predictions)
 
-        # 求动作函数的预测值
+        # 求动作函数的预测值,先求数据应该截取的位置
         incidents=tf.range(bat_size)*tf.shape(self.predictions)[1]+self.actions_pl
-        # ???需要后面解释一下
 
         # 先将predictions转换成一维向量然后切片
         self.act_predict=tf.gather(tf.reshape(self.predictions,[-1]),incidents)
-        
+        #训练参数
+        model.compile(optimizer=tf.keras.optimizers.RMSprop(0.00025,0.99,0.0,1e-7),loss=tf.keras.losses.MSE(self.y_pl,self.act_predict))
         # 计算损失函数
         # self.losses=tf.math.squared_difference(self.y_pl,self.act_predict)
         # self.loss=tf.math.reduce_mean(self.losses)
         # 上面两步计算可以简化为直接一步计算
-        self.loss=tf.keras.losses.MSE(self.y_pl,self.act_predict)
+        # self.loss=tf.keras.losses.MSE(self.y_pl,self.act_predict)
 
         # RMSprop优化器
-        self.optimizer=tf.keras.optimizers.RMSprop(0.00025,0.99,0.0,1e-7)
-        self.train_op=self.optimizer.minimize(self.loss)
+        # self.optimizer=tf.keras.optimizers.RMSprop(0.00025,0.99,0.0,1e-7)
+        # self.train_op=self.optimizer.minimize(self.loss)
 
         # tensorboard显出训练总结
         log_dir="logs/fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
-        tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
+        writer = tf.summary.create_file_writer(log_dir)
+        with writer.as_default():
+            tf.summary.scalar("my_metric",0.5)
+            writer.flush()
+        # tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
 
     # def predict(self,)
     # 这里需要考虑一下预测是怎么搞的
